6be532f8551a4679f7fa930b0bc2620ee58d365a
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-01-09 16:54:12 +0100

    Added empty flair handling

--- src/index.py
+++ src/index.py
@@ -5,7 +5,6 @@ import logging
 from datetime import timedelta, datetime
 from sys import exit
 from prawcore import NotFound
-from praw.models import ModAction
 
 reddit = praw.Reddit(
     client_id=getenv("MMB_CLIENT_ID"),
@@ -53,24 +52,40 @@ class DatabaseHelper:
 
 
 def run():
-    sub = reddit.subreddit(getenv('MMB_SUBREDDIT'))
+    sub = reddit.subreddit(getenv("MMB_SUBREDDIT"))
 
     # listening for post flair edits
-    for action in sub.mod.log(action='editflair'):
+    for action in sub.mod.log(action="editflair"):
+        """
+        'description': None, 'target_body': None, 'mod_id36': 'v16iabx', 'created_utc': 1604159047.0, 'subreddit': 'xeothtest', 'target_title': 'ps 3', 'target_permalink': '/r/xeothtest/comments/hhz1a4/ps_3/', 'subreddit_name_prefixed': 'r/xeothtest', 'details': 'flair_edit', 'action': 'editflair', 'target_author': 'alteoth', 'target_fullname': 't3_hhz1a4', 'sr_id36': '2i4bah', 'id': 'ModAction_e9427abc-1b8f-11eb-91ee-9e594cb2ce46', '_mod': 'Xeoth'}b
+        """
         # we only want flair changes on posts
         if not action.target_permalink:
             continue
 
         # first, we need to get the flair for a specified user
+        # we're fetching one but it's still not subscriptable
+        for f in sub.flair(redditor=action.target_author):
+            flair = f
+
+        flair_class = flair.flair_css_class
+        if not flair_class:
+            sub.mod.flair.set(redditor=flair.user, css_class=f"1s {flair.target_fullname[3:]}")
+            continue
+
+        # the format of the flair_css_class will look like Xs AAAAA BBBBB CCCCC
+        flair_list = flair_class.split(' ')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if reddit.read_only:
-        logging.critical('The reddit instance is read-only! Terminating...')
+        logging.critical("The reddit instance is read-only! Terminating...")
         exit(1)
 
     try:
         run()
     except NotFound:
-        logging.fatal('The moderation log is not available to the signed in user. Are you using a mod account?')
+        logging.fatal(
+            "The moderation log is not available to the signed in user. Are you using a mod account?"
+        )
         exit(1)
